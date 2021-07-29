83a101deba32162dd11357e1c8cf28554c492765
likorsi <li_korsi@mail.ru>
2021-07-29 21:47:33 +0300

    change the same fragments of code to templates

--- js/script.js
+++ js/script.js
@@ -26,12 +26,47 @@ function changeContentForDevice() {
 }
 
 function changeSlideWidth() {
-    slideWidth = carousel.children.item(0).offsetWidth
+    slideWidth = carousel.querySelector('li').offsetWidth
     let wrapper = document.querySelector(".carousel-wrapper")
     position = 0
     wrapper.style.width = slideWidth + 'px'
     carousel.style.marginLeft = 0
 }
+
+function makeCardProductTemplates(parent, template, number) {
+    for (let i = 0; i < number; i++) {
+        let element = template.cloneNode(true)
+        element.href = "#"
+        element.className = "link-card-product"
+        element.querySelector("p").innerHTML = `Louis XVI ATHOS<br>165 000 руб.`
+        let img = element.querySelector("img")
+        img.src = "./static/img/wrist-watch.png"
+        img.alt = "наручные часы Louis XVI ATHOS"
+        parent.appendChild(element)
+    }
+}
+
+function makeBrandsTemplates(parent, template, number) {
+    for (let i = 0; i < number; i++) {
+        let element = template.cloneNode(true)
+        let img = element.querySelector("img")
+        img.src = "./static/img/brand.svg"
+        img.alt = "логотип бренда Cocofine Jewelry"
+        parent.appendChild(element)
+    }
+}
+
+let template = document.querySelector("#card-product-template").content.querySelector('a')
+let parent = document.querySelector(".new-collection .card-product")
+makeCardProductTemplates(parent, template, 3)
+parent = document.querySelector(".goods > .card-product")
+makeCardProductTemplates(parent, template, 8)
+template = document.querySelector("#card-product-template-li").content.querySelector('li')
+parent = document.querySelector(".carousel-gallery")
+makeCardProductTemplates(parent, template, 3)
+parent = document.querySelector(".brands ul")
+template = document.querySelector("#brands-template").content.querySelector('li')
+makeBrandsTemplates(parent, template, 4)
 
 changeContentForDevice()
 window.addEventListener("resize", changeContentForDevice)
